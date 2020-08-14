b5129371124ee910d9136d8f63eb3318e966f672
luiz009988 <luiz_gabriel@estudante.sc.senai.br>
2020-08-13 21:31:42 -0300

    aula 13_08 exeption 2

--- src/projeto_13_08/projeto_13_08.java
+++ src/projeto_13_08/projeto_13_08.java
@@ -14,10 +14,18 @@ import java.util.Scanner;
  * @author Luiz
  */
 public class projeto_13_08 {
+    
+    
     public static void main(String[] args) {
+        metodo1();
+        System.out.println("termino do programa");
+        }
+    
+    
+    public static void metodo1 (){
         Scanner sc= new Scanner(System.in);
-        
-        
+        System.out.println("iniciando metodo 1");
+        metodo2();
         try{
             
         String [] vetor = sc.nextLine().split("  ");
@@ -30,6 +38,10 @@ public class projeto_13_08 {
         catch(ArrayIndexOutOfBoundsException e){
             System.out.println("tenta mais vacilao");
         }
-            System.out.println("fim");
-        }
+            System.out.println("fim metodo1");
+    }
+    
+    public static void metodo2(){
+        System.out.println("pau vai cume");
+    } 
 }
